7b9c41ee3516eae6b402f26c393fbd4fed46f86e
zhh <502222517@qq.com>
2016-06-03 18:23:51 +0800

    添加manage
    添加manage

--- WebRoot/manage/README.md
+++ WebRoot/manage/README.md
@@ -1,8 +1,8 @@
 # 网站说明
 	1.使用java mybaits+springmvc 架构。
 	2.前端使用bower管理包，grunt打包项目文件。
-	3.使用bootstrap 插件作为基础ui库。
-	4.app 为前端展现系统
+	3.使用jqueryEasyUI 插件作为基础ui库。
+	4.manage 为系统管理后台
 	
 
 # 参考网站
@@ -22,7 +22,7 @@
 	
 # 启动
 	1.运行tomcat
-	2.访问: http://192.168.1.6:9999/blogs/app/static/index.html
+	2.访问: http://192.168.1.6:9999/blogs/manage/static/index.html
 	
 	
 	
---
+++ src/com/zhh/ssm/service/ArticleService.java
@@ -0,0 +1,74 @@
+/**   
+ * @Title: ArticleService.java 
+ * @Description: TODO
+ * @author zhh
+ * @date 2016年6月2日 下午3:37:13 
+ * @version V1.0   
+ */
+package com.zhh.ssm.service;
+
+import java.util.List;
+
+import com.zhh.ssm.po.base.Article;
+
+/** 
+ * @Title: 文章管理 
+ * @ClassName: ArticleService 
+ * @Description: TODO 
+ * @author zhh
+ * @date 2016年6月2日 下午3:37:13  
+ */
+public interface ArticleService {
+	
+	/**
+	* @Title: findArticleById 
+	* @Description: 根据id获取文章
+	* @param @param id
+	* @param @return
+	* @param @throws Exception    设定文件 
+	* @return Article    返回类型 
+	* @throws
+	 */
+	public Article findArticleById(int id) throws Exception;
+	
+	/**
+	 * 
+	* @Title: 插入文章
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @param article
+	* @param @return
+	* @param @throws Exception    设定文件 
+	* @return int    返回类型 
+	* @throws
+	*/
+	public int insertArticle(Article article) throws Exception;
+	
+	/**
+	* @Title: 更新文章
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @param article
+	* @param @return
+	* @param @throws Exception    设定文件 
+	* @return int    返回类型 
+	* @throws
+	*/
+	public int updateArticle(Article article) throws Exception;
+	
+	/**
+	* @Title: 删除文章 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @param article
+	* @param @return
+	* @param @throws Exception    设定文件 
+	* @return int    返回类型 
+	* @throws
+	*/
+	public int deleteArticle(int id) throws Exception;
+	
+	
+	
+	
+	
+	
+
+}
